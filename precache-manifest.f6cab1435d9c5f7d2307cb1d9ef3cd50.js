b0a84d5e5dd799510126d46912dbd5b7305e8170
DeSean Ward <dward1711@yahoo.com>
2020-02-27 15:06:16 -0600

    Updates

---
+++ precache-manifest.f6cab1435d9c5f7d2307cb1d9ef3cd50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdec8a4b3fe01f4b09e2049c5d6dfc29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a316c8af4d75200dd19b",
+    "url": "/monsters-rolodex/static/css/main.698d1a49.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a316c8af4d75200dd19b",
+    "url": "/monsters-rolodex/static/js/main.16dd1970.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
