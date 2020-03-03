ff78aa7f88408e9952e8071d0248ff12a0ef56b6
DeSean Ward <dward1711@yahoo.com>
2020-03-03 10:07:20 -0600

    Updates

--- precache-manifest.d4e4e6af794e5472bf8f0c2156023e79.js
+++ precache-manifest.d4e4e6af794e5472bf8f0c2156023e79.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8f08358c1fcb27c9ecff93998f61f5dd",
+    "revision": "c7cd8d14489e5b9677c1bb74cfba22de",
     "url": "/monsters-rolodex/index.html"
   },
   {
-    "revision": "5c72adfae95367b430ce",
+    "revision": "610d96c963969b65e753",
     "url": "/monsters-rolodex/static/css/main.39938bb0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monsters-rolodex/static/js/2.615db946.chunk.js.LICENSE"
   },
   {
-    "revision": "5c72adfae95367b430ce",
-    "url": "/monsters-rolodex/static/js/main.16dd1970.chunk.js"
+    "revision": "610d96c963969b65e753",
+    "url": "/monsters-rolodex/static/js/main.738ad19b.chunk.js"
   },
   {
     "revision": "9e014af7b12dc10ef867",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.8751a72225c9fcd9a1432f4ec0f0d7ba.js"
+  "/monsters-rolodex/precache-manifest.d4e4e6af794e5472bf8f0c2156023e79.js"
 );
 
 self.addEventListener('message', (event) => {
